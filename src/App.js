5018ac80dca2931e1bf3c17b8bdb69bc49d09761
ShanKyTech <admin@shankytech.com>
2024-06-02 20:10:36 -0400

    changed app text using feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am doing good.
+          I was changed on feature branch
         </a>
         <p>Learn React</p>
       </header>
